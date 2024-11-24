d58bec3f1e603edc94f4ca3e76985dc145931516
Nourine Mohammed Nadir <nounadir@gmail.com>
2024-11-24 10:33:18 +0100

    add files

---
+++ AutoEncoder/main.py
@@ -0,0 +1,79 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from test import evaluate_model
+from get_data import get_data
+from train_AE import train_AE
+from test_AE import test_AE
+from train import train
+from utils import *
+import json
+# HYPERPARAMETERS
+with (open('params.json', 'r') as f):
+    params = json.load(f)["parameters"]
+
+    _latent_dim, ROOT_FOLDER, TARGET_FOLDER,\
+    _load_and_resize,_display,  TARGET_SIZE, SAVE_FILE, \
+    _lr, _fc1_dims, _fc2_dims, _fc3_dims,_fc4_dims, _epochs, _batch_size, _encoder_model_path,\
+    _full_model_path,_load_model, _train, _test \
+    =(params[key] for key in
+     list(params.keys())
+     )
+
+
+if __name__ == '__main__':
+
+    train_images, test_images, y_train, y_test =\
+                     get_data(root_folder=ROOT_FOLDER,
+                     target_folder=TARGET_FOLDER,
+                     target_size=TARGET_SIZE,
+                     save_file=SAVE_FILE,
+                     load_and_resize=_load_and_resize)
+
+    print(train_images.shape, test_images.shape)
+    print(y_train.shape, y_test.shape)
+
+    if _train:
+        print('------ Train AutoEncoder ------')
+        full_model, trained_encoder, original_images, gen_images = train_AE(data= train_images,
+              latent_dim = _latent_dim,
+              load_model=_load_model,
+              encoder_model_path=_encoder_model_path,
+              full_model_path=_full_model_path,
+              epochs=_epochs,
+              batch_size=_batch_size,
+              lr=_lr,)
+
+        nb_imgs = 2
+        display_images(np.squeeze(gen_images)[:nb_imgs],rows=int(nb_imgs/2),cols=2)
+        display_images(np.squeeze(original_images)[:nb_imgs],rows=int(nb_imgs/2),cols=2)
+
+    if _test:
+        print('------ Test AutoEncoder ------')
+        test_AE(data= test_images,
+             latent_dim=_latent_dim,
+             encoder_model_path=_encoder_model_path,
+             full_model_path=_full_model_path)
+
+
+    print('------ Train Naive Net ------')
+    trained_net, train_loss = train(features=train_images,
+                                    labels=y_train,
+                                    lr=_lr,
+                                    fc1_dims=_fc1_dims,
+                                    fc2_dims=_fc2_dims,
+                                    fc3_dims=_fc3_dims,
+                                    fc4_dims=_fc4_dims,
+                                    latent_dim=_latent_dim,
+                                    encoder_model_path=_encoder_model_path,
+                                    trained_encoder= trained_encoder,
+                                    n_epochs=_epochs*25,
+                                    batch_size=_batch_size)
+
+    print('------ Train Naive Net ------')
+    accuracy = evaluate_model(net=trained_net,
+                              latent_dim=_latent_dim,
+                              encoder_model_path=_encoder_model_path,
+                              trained_encoder=trained_encoder,
+                              features=test_images,
+                              labels=y_test,
+                              train_loss=train_loss)
---
+++ AutoEncoder/test.py
@@ -0,0 +1,83 @@
+import torch as T
+import numpy as np
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, roc_auc_score, roc_curve, auc
+import matplotlib.pyplot as plt
+import seaborn as sns
+from typing import Tuple
+from models import Naive_net, Encoder
+
+def evaluate_model(net,
+                   latent_dim:int,
+                   encoder_model_path,
+                   trained_encoder,
+                   features: np.ndarray,
+                   labels: np.ndarray,
+                   train_loss
+                   ) -> Tuple[float, float]:
+
+    # Convert to tensors
+    labels = T.tensor(labels, device=net.device, dtype=T.float32)
+    # encoder = Encoder(latent_dim=latent_dim)
+    # encoder.load_model(encoder_model_path, map_location=T.device('cuda'))
+    encoder = trained_encoder
+
+    with T.no_grad():  # Add this to prevent gradient computation for encoder
+        images = T.FloatTensor(features.astype(np.float32) / 255.0)
+        if len(images.shape) == 3:
+            images = images.unsqueeze(1)
+        images = images.to(encoder.device)
+        print(f'images shape in training : {images.shape}')
+
+        # Get encoded features
+        encoded_features = encoder(images)
+        # Detach to prevent backward through encoder
+        encoded_features = encoded_features.detach()
+
+
+
+
+
+
+    # Evaluate model
+    net.eval()  # Set to evaluation mode
+    with T.no_grad():
+        predictions = net(encoded_features)
+        pred_labels = T.argmax(predictions, dim=1).cpu().numpy()
+        labels_1D = T.argmax(labels, dim=1).cpu().numpy()
+
+
+        # Calculate accuracy
+
+        accuracy = accuracy_score(labels_1D, pred_labels)
+
+        # Calculate loss
+        loss = net.loss(labels, predictions).item()
+
+        # Calculate f1score
+        f1score = f1_score(labels_1D, pred_labels, average='macro')
+
+        # Calculate roc_auc_score
+        roc_auc = roc_auc_score(labels_1D, pred_labels, average='macro')
+
+
+
+        # Create and plot confusion matrix
+        cm = confusion_matrix(labels_1D, pred_labels)
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+        plt.title(f'Confusion Matrix, Accuracy = {accuracy:.4f}')
+        plt.ylabel('True Label')
+        plt.xlabel('Predicted Label')
+        plt.savefig('confusion_matrix.png')
+        plt.show()
+
+        plt.plot(train_loss)
+        plt.savefig('loss.png')
+        plt.show()
+
+    print(f"\nOverall Test Results:")
+    print(f"Loss: {loss:.4f}")
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"f1_Score: {f1score:.4f}")
+    print(f"roc_auc_score: {roc_auc:.4f}")
+    return accuracy
