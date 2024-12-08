d31871b38367757288a4eea284f0bc19dc40ec4c
Nourine Mohammed Nadir <nounadir@gmail.com>
2024-12-08 08:51:24 +0100

    update

--- ViT/test.py
+++ ViT/test.py
@@ -1,10 +1,11 @@
 import torch as T
 import numpy as np
-from vit import ViT
+from sklearn.model_selection import train_test_split
+from ViT.ViT_model import ViT
+from tqdm import tqdm
 from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, roc_auc_score
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 def test(
         model,
         images,
@@ -13,49 +14,59 @@ def test(
         model_path,
         load_model
 ):
-    DEVICE = T.device('cuda' if T.cuda.is_available() else 'cpu')
     with T.no_grad():  # Add this to prevent gradient computation for encoder
         images = T.FloatTensor(np.array(images).astype(np.float32) / 255.0)
     if len(images.shape) == 3 :
         images = T.unsqueeze(images, dim=1)
-    print(images.shape)
 
-
-    labels = T.tensor(np.argmax(np.array(labels), axis=-1), device=DEVICE, dtype=T.long)
-
-    criterion = T.nn.CrossEntropyLoss()
+    DEVICE = model.device
+    labels = T.tensor(np.array(labels), device=DEVICE, dtype=T.float32)
+    print(type(images))
+    test_dataset = T.utils.data.TensorDataset(images, labels)
+    test_loader = T.utils.data.DataLoader(test_dataset,
+                                           batch_size=config['batch_size'],
+                                           shuffle=False)
+    all_preds = []
+    all_labels = []
+    model.eval()  # Set model to evaluation mode
     with T.no_grad():
-        predictions = model(images)
-        pred_labels = T.argmax(predictions, dim=1).cpu().numpy()
-        labels_1D = labels.cpu().numpy()
-        print(predictions.shape)
-        # Calculate accuracy
+        for inputs, batch_labels in test_loader:
+            inputs = inputs.to(DEVICE)
+            batch_labels = batch_labels.to(DEVICE)
 
-        accuracy = accuracy_score(labels_1D, pred_labels)
+            predictions = model(inputs)
+            pred_labels = predictions.cpu().numpy()
 
-        # Calculate loss
-        loss = criterion(predictions, labels).item()
+            all_preds.extend(pred_labels)
+            all_labels.extend(batch_labels.cpu().numpy())
 
-        # Calculate f1score
-        f1score = f1_score(labels_1D, pred_labels, average='macro')
+    labels_1D = T.argmax(labels, dim=1).cpu().numpy()
+    all_preds_1D = np.argmax(all_preds,axis=1)
 
-        # Calculate roc_auc_score
-        # roc_auc = roc_auc_score(labels_1D, pred_labels, average='macro')
+    # Calculate accuracy
+    accuracy = accuracy_score(labels_1D, all_preds_1D)
 
-        # Create and plot confusion matrix
-        cm = confusion_matrix(labels_1D, pred_labels)
-        plt.figure(figsize=(10, 8))
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-        plt.title(f'Confusion Matrix, Accuracy = {accuracy:.4f}')
-        plt.ylabel('True Label')
-        plt.xlabel('Predicted Label')
-        plt.savefig(f'confusion_matrix {accuracy:.4f}.png')
-        plt.show()
+    # Calculate loss
+    loss = model.criterion(T.tensor(np.array(all_labels), device=DEVICE),
+                           T.tensor(np.array(all_preds), device=DEVICE)).item()
+
+    # Calculate f1score
+    f1score = f1_score(labels_1D, all_preds_1D, average='macro')
+
+
+
+    # Create and plot confusion matrix
+    cm = confusion_matrix(labels_1D, all_preds_1D)
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title(f'Confusion Matrix, Accuracy = {accuracy:.4f}')
+    plt.ylabel('True Label')
+    plt.xlabel('Predicted Label')
+    plt.savefig(f'confusion_matrix {accuracy:.4f}.png')
+    plt.show()
 
     print(f"\nOverall Test Results:")
     print(f"Loss: {loss:.4f}")
     print(f"Accuracy: {accuracy:.4f}")
     print(f"f1_Score: {f1score:.4f}")
-
-    model.save_model(model_path+ ' last_model')
     return
--- ViT/train.py
+++ ViT/train.py
@@ -1,10 +1,8 @@
 import torch as T
 import numpy as np
 from sklearn.model_selection import train_test_split
-from vit import ViT
+from ViT_model import ViT
 from tqdm import tqdm
-
-
 
 def train(
         images,
@@ -13,37 +11,36 @@ def train(
         model_path,
         load_model
 ):
-    DEVICE = T.device('cuda' if T.cuda.is_available() else 'cpu')
     with T.no_grad():  # Add this to prevent gradient computation for encoder
         images = T.FloatTensor(np.array(images).astype(np.float32) / 255.0)
     if len(images.shape) == 3 :
         images = T.unsqueeze(images, dim=1)
 
     model = ViT(
-        image_size = images.shape[-1],
-        patch_size = 8,
-        num_classes = 2,
-        dim = 64,
-        depth = 6,
-        heads = 4,
-        mlp_dim = 128,
-        dropout = 0.2,
-        emb_dropout = 0.2,
-        channels = 1,
-    ).to(DEVICE)
+        lr=config['lr'],
+        in_channels=images.shape[1],
+        img_size=images.shape[-1],
+        patch_size=config['patch_size'],
+        emb_dim=config['emb_dim'],
+        n_heads=config['n_heads'],
+        n_layers=config['n_layers'],
+        mlp_dim=config['mlp_dim'],
+        dropout=config['dropout'],
+        out_dim=len(np.unique(labels)),
 
-
-    if load_model:
-        model.load_model(model_path+ ' best_model')
-    labels = T.tensor(
-        np.argmax(np.array(labels),axis=-1)
     )
+    if load_model:
+        model.load_model(model_path)
+    # labels = np.argmax(labels, axis=1)
+    labels = T.tensor(np.array(labels), dtype=T.float32)
 
     X_train, X_val, y_train, y_val = train_test_split(
         images, labels,
         test_size=0.2,  # 20% for validation
         random_state=42  # for reproducibility
     )
+    # y_train = T.tensor(np.array(y_train), dtype=T.float32)
+    # y_val = T.tensor(np.array(y_val), dtype=T.float32)
 
     train_dataset = T.utils.data.TensorDataset(X_train, y_train)
     train_loader = T.utils.data.DataLoader(train_dataset,
@@ -52,63 +49,48 @@ def train(
     val_dataset = T.utils.data.TensorDataset(X_val, y_val)
     val_loader = T.utils.data.DataLoader(val_dataset,
                                            batch_size=config['batch_size'],
-                                           shuffle=True)
+                                           shuffle=False)
     best_loss = 9999
     train_losses = []
     val_losses = []
-    optimizer = T.optim.AdamW(model.parameters(), lr=config['lr'])
-    scheduler = T.optim.lr_scheduler.LambdaLR(
-        optimizer,
-        lr_lambda=lambda epoch: max(0.995 ** epoch, 1e-1)
-    )
-    criterion = T.nn.CrossEntropyLoss()
     for epoch in tqdm(range(config['epochs'])):
         epoch_losses = []
         total_train_loss = 0
 
         model.train()
         for step, (inputs, labels) in enumerate(train_loader):
-            inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
-            optimizer.zero_grad()
+            inputs, labels = inputs.to(model.device), labels.to(model.device)
+            model.optimizer.zero_grad()
 
             outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            loss = model.criterion(outputs, labels)
             loss.backward()
-            optimizer.step()
+            model.optimizer.step()
             epoch_losses.append(loss.item())
             total_train_loss += loss.item()
 
+        model.eval()
+        total_val_loss = 0
+        for step, (inputs, labels) in enumerate(val_loader):
+            inputs, labels = inputs.to(model.device), labels.to(model.device)
+            outputs = model(inputs)
+            loss = model.criterion(outputs, labels)
+            total_val_loss += loss.item()
+            epoch_losses.append(loss.item())
 
+        avg_train_loss = total_train_loss / len(train_loader)
+        avg_val_loss = total_val_loss / len(val_loader)
 
-        with T.no_grad():
-            total_val_loss = 0
-            for step, (inputs, labels) in enumerate(val_loader):
-                inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
+        train_losses.append(avg_train_loss)
+        val_losses.append(avg_val_loss)
 
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                total_val_loss += loss.item()
-                epoch_losses.append(loss.item())
-
-                _, predicted = T.max(outputs, 1)
-                correct = (predicted == labels).sum().item()
-                accuracy = correct / len(labels)
-                print(f'Batch Accuracy: {accuracy:.4f}')
-
-
-            avg_train_loss = total_train_loss / len(train_loader)
-            avg_val_loss = total_val_loss / len(val_loader)
-
-            train_losses.append(avg_train_loss)
-            val_losses.append(avg_val_loss)
-
-            if avg_val_loss < best_loss:
-                model.save_model(model_path+ ' best_model')
-        if epoch % 5 == 0:
+        if avg_val_loss < best_loss:
+            model.save_model(model_path+ ' best_model')
+        if epoch % 1 == 0:
+            model.lr_decay()
             print(f'Train Loss: {avg_train_loss:.6f},'
                   f' Validation Loss: {avg_val_loss:.6f},'
-                  )
-
+                  f'lr {model.get_lr():.5f}' )
 
     model.save_model(model_path+ ' last_model')
     return model
